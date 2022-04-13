d807e8af91565d646e0bcfdaef7457f057850485
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2022-04-13 10:08:33 +0700

    konfigurasi api

--- routes/api.php
+++ routes/api.php
@@ -37,7 +37,7 @@ Route::middleware('auth:sanctum')->group(function() {
     Route::post('logout', [UserController::class, 'logout']);
     // diaries
     Route::post('diary', [DiaryController::class, 'store']);
-    Route::post('diary/{id}', [DiaryController::class, 'update']);
+    Route::put('diary/{id}', [DiaryController::class, 'update']);
     Route::delete('diary/{id}', [DiaryController::class, 'destroy']);
     // courses
     Route::post('course', [CourseController::class, 'store']);
